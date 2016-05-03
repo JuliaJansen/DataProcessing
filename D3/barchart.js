771c8e194b64a40389efe8d1e0d35f014c8b8d8f
JuliaJansen <juliartjansen@gmail.com>
2016-05-03 09:36:50 +0200

    Herordening map

--- D3/barchart.js
+++ D3/barchart.js
@@ -10,9 +10,119 @@ window.onload = function() {
 
 	// load json with d3
 	var json = "pop_in_largest_city_2014.json";
-	d3.json(json, function(d) {
+	d3.json(json, function (data) {
 
-		// log json data
-		console.log(d);
+		var temp = data.points;
+		var data = [];
+
+		// use only data points for countries with information
+		for (var i = 0; i < temp.length; i++) {
+			if (temp[i].percentage > 0) {
+				data.push(temp[i]);
+			}
+		}
+
+		data.sort(function(a, b) {
+    		return parseFloat(b.percentage) - parseFloat(a.percentage);
+		});
+
+		console.log("data length = ", data.length);
+
+		margin
+		var margin = {top: 20, right: 30, bottom: 30, left: 30};
+
+		// width of chart and height of bars
+		var width = 800 - margin.left - margin.right, 
+			height = margin.top + 20 * 159,
+			barheight = 20;
+
+		// define how to scale the bars
+		var x = d3.scale.linear()
+    		.range([0, width]);
+
+    	// define scale of y-ax
+		var y = d3.scale.linear()
+    		.range([height, 0]);
+
+    	// x-axis
+    	var xAxis = d3.svg.axis()
+    		.scale(x)
+   	 		.orient("top")
+   	 		.ticks(10, "%");
+
+   	 	var yAxis = d3.svg.axis()
+   	 		.scale(y)
+    		.orient("left")
+    		.ticks(NaN);
+
+    	// make tooltip
+  		var div = d3.select("body").append("div")	
+    		.attr("class", "tooltip")				
+    		.style("visibility", "hidden");
+
+    	// select chart
+		var chart = d3.select(".chart")
+		    .attr("width", width + margin.left) 
+		    .attr("height", height + margin.top)
+		  .append("g")
+    		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+		var bar = chart.selectAll("g")
+    		.data(data)
+		  .enter().append("g")
+    		.attr("transform", function(d, i) { return "translate(0," + i * barheight + ")"; });
+
+		// add a g element for each data point
+		bar.append("rect")
+			.attr("y", function(d, i) { return i })
+	  		.attr("width", function(d) { return x(0.01 * d.percentage) })
+			.attr("height", barheight - 1)
+			.on("mouseover", function(d) { 
+					d3.select(this).style("fill",  "#e68a00");
+					div.style("visibility", "visible");
+					var num = 1 * d.percentage;
+					div.html(num.toFixed(2) + "%")
+					   .style("left", (d3.event.pageX) + "px")
+					   .style("top", (d3.event.pageY - 28) + "px");
+
+				})
+			.on("mouseout", function(d) { d3.select(this).style("fill", "#009999") });
+
+	    bar.append("text")
+	    	.attr("id", "country")
+	    	.attr("x", 6)
+	    	.attr("y", function(d, i) { return i + 5 })
+	    	.attr("dy", ".75em")
+	    	.text(function(d) { return d.country })
+	    	.style("text-anchor", "start");
+	    	.on("mouseover", function(d) { 
+					d3.select("rect").style("fill",  "#e68a00");
+					// div.style("visibility", "visible");
+					// var num = 1 * d.percentage;
+					// div.html(num.toFixed(2) + "%")
+					//    .style("left", (d3.event.pageX) + "px")
+					//    .style("top", (d3.event.pageY - 28) + "px");
+
+				})
+			.on("mouseout", function(d) { d3.select(this).style("fill", "#009999") });
+
+		// add x axis
+		chart.append("g")
+		    .attr("class", "x axis")
+		    .attr("transform", function(d, i) { return "translate(0," + i * barheight + ")"})
+		    .call(xAxis);
+
+		// add y axis
+		chart.append("g")
+      		.attr("class", "y axis")
+      		.call(yAxis)
+      	  .append("text")
+    		.attr("transform", "rotate(90)")
+    		.attr("y", 15)
+    		.attr("x", 10)
+    		.attr("dy", ".71em")
+    		.style("text-anchor", "start")
+    		.text("Country");
+
 	});
-}
+};
