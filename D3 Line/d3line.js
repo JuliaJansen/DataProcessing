d895521ea2327a028a61b142bf787771c7d787c5
JuliaJansen <juliartjansen@gmail.com>
2016-05-11 17:38:18 +0200

    update
    LIJN ZICHTBAAR :dancers:

--- D3 Line/d3line.js
+++ D3 Line/d3line.js
@@ -16,51 +16,29 @@ function makeGraph(error, temp, rain) {
 	// check for errors while loading the page
 	if (error) throw error;
 
- 	// save temperature data in array
- 	tempdata = temp.points;
+ 	var temporary = temp;
+ 	var tempdata_irn_2010 = [];
 
-	console.log(tempdata);
-		
+ 	for (var i = 0; i < temporary.length; i++) {
+ 		if (temporary[i].country_code == "IRN") {
+ 			if (temporary[i].year == "2010") {
+ 				temporary[i].month = +temporary[i].month;
+ 				temporary[i].temperature = +temporary[i].temperature;
+ 				tempdata_irn_2010.push(temporary[i]);
+ 				// console.log(temporary[i]);
+ 			}
+ 		}
+ 	}
 
-	// 	// round raindata
-	// 	var num = +d.temperature;
-	// 	d.temp = num.toFixed(2);
+ 	console.log(tempdata_irn_2010);
 
-	// 	// use country as index
-	// 	tempdata.push({
-	// 			country: d.country,
-	// 			month : d.month,
-	// 			temp : d.temp,
-	// 			year : d.year
-	// 		})
-	// })
-
-	// // save rainfall data in array
-	// var raintemporary = rain.points;
-	// var raindata = {};
-
-	// raintemporary.forEach(function(d) {
-	// 	if (d.country_code == 'NLD') {
-	// 		d.country = "Netherlands"
-	// 	}
-	// 	var num = +d.rainfall;
-	// 	d.rain = num.toFixed(2);
-
-	// 	// use country as index
-	// 	raindata[d.country] = {
-	// 			month : d.month,
-	// 			rain : d.rain,
-	// 			year : d.year
-	// 		}
-	// })
-
-	var margin = {top: 30, right: 30, bottom: 30, left: 30},
+	var margin = {top: 30, right: 30, bottom: 30, left: 50},
     	width = 960 - margin.left - margin.right,
     	height = 500 - margin.top - margin.bottom;
 
-	var formatDate = d3.time.format("%d-%b-%y");
+	// var formatDate = d3.time.format("%d-%b-%y");
 
-	var x = d3.time.scale()
+	var x = d3.scale.linear()
 	    .range([0, width]);
 
 	var y = d3.scale.linear()
@@ -75,8 +53,8 @@ function makeGraph(error, temp, rain) {
 	    .orient("left");
 
 	var line = d3.svg.line()
-	    .x(function(d) { return x(+d.month); })
-	    .y(function(d) { return y(+d.temperature); });
+	    .x(function(d) { return x(d.month); })
+	    .y(function(d) { return y(d.temperature); });
 
 	var svg = d3.select("#content").append("svg")
 		.attr("width", 960)
@@ -84,39 +62,41 @@ function makeGraph(error, temp, rain) {
 	  .append("g")
 	  	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-	x.domain(d3.extent(tempdata, function(d) { return d.month; }));
-  	y.domain(d3.extent(tempdata, function(d) { return d.temp; }));
+	x.domain(d3.extent(tempdata_irn_2010, function(d) { return d.month; }));
+  	y.domain(d3.extent(tempdata_irn_2010, function(d) { return d.temperature; }));
 
-  	svg.append("g")
-    	.attr("class", "y axis")
-    	.call(yAxis)
-      .append("text")
-    	.attr("transform", "rotate(-90)")
-		.attr("y", -15)
-		.attr("dy", ".71em")
-		.style("text-anchor", "end")
-		.text("Average Temperature (\&\#8451)");
-
+  	// add X axis
     svg.append("g")
 		.attr("class", "x axis")
 		.attr("transform", "translate(0," + height + ")")
 		.call(xAxis);
 
-	var year = svg.selectAll(".year")
-      	.data(tempdata)
-      .enter().append("g")
-      	.attr("class", "year");
+	// Add y axis
+  	svg.append("g")
+    	.attr("class", "y axis")
+    	.call(yAxis)
+      .append("text")
+    	.attr("transform", "rotate(-90)")
+		.attr("y", -40)
+		.attr("dy", ".71em")
+		.style("text-anchor", "end")
+		.text("Average Temperature (Celsius)");
 
-  	year.append("path")
+    // Add line
+  	svg.append("path")
+  		.datum(tempdata_irn_2010)
         .attr("class", "line")
-        .attr("d", function(d) { if (d.country_code == "IRN" && d.year == 2010) { console.log(d.country_code); return line; }});
+        .attr("d", line);
 
-	// year.append("text")
-	// 	.datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
-	// 	.attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")"; })
-	// 	.attr("x", 3)
-	// 	.attr("dy", ".35em")
-	// 	.text(function(d) { return d.name; });
+    // Add the scatterplot
+    svg.selectAll("dot")
+        .data(tempdata_irn_2010)
+      .enter().append("circle")
+      	.attr("class", "circle")
+        .attr("r", 3.5)
+        .attr("cx", function(d) { return x(d.month); })
+        .attr("cy", function(d) { return y(d.temperature); });
+
 
 }
 
